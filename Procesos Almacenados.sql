049b1057678f09c73be25afc908e8979b4b0b8c0
Danmakora <joshegalaxy11@gmail.com>
2022-02-19 18:27:15 -0600

    Proceso No. 2 Completo

--- Procesos Almacenados.sql
+++ Procesos Almacenados.sql
@@ -1,6 +1,6 @@
+/* Proceso No. 1: Guardar Subscriptor */
 Drop Procedure if Exists SP_Guardar_Subscriptor;
 
-/* Proceso No. 1: Guardar Subscriptor */
 Delimiter //
 
 Create Procedure SP_Guardar_Subscriptor(
@@ -28,4 +28,42 @@ Begin
 		set apellidos = p_apellidos;
     
 	commit;
-End
+End;
+
+/* Proceso No. 2: Guardar Producto */
+
+Drop Procedure if Exists SP_Guardar_Producto;
+
+Delimiter //
+
+Create Procedure SP_Guardar_Producto(
+	in p_id_producto 	int,
+    in p_nombre			varchar(45),
+    in p_descripcion	varchar(45),
+    in p_precio_costo 	decimal(12,2),
+    in p_precio_venta	decimal(12,2)
+)
+
+Begin
+	declare v_id_producto int; 
+	declare v_nom_prod varchar(45); 
+	declare v_desc_prod varchar(45) ;
+	declare v_precio_costo decimal(12,2);
+    declare v_precio_venta decimal(12,2);
+
+	set v_id_producto 		= p_id_producto;
+	set v_nom_prod			= p_nombre; 
+	set v_desc_prod			= p_descripcion;
+	set v_precio_costo		= p_precio_costo;
+    set v_precio_venta 		= p_precio_venta;
+		
+    update bd_sample.tbl_productos
+		set id_producto = p_id_producto;
+        set nombre = p_nombre;
+        set descripcion = p_descripcion;
+        set precio_costo = p_precio_costo;
+        set precio_venta = (p_precio_costo)*1.25;
+    
+	commit;
+End;
+
