2c1d6f61f5cc7589cdf313c8b0323f33d9464e48
Danmakora <joshegalaxy11@gmail.com>
2022-02-28 21:19:16 -0600

    Fix-a-majig!

--- Ejercicio 2.3 EdC.sql
+++ Ejercicio 2.3 EdC.sql
@@ -21,10 +21,11 @@ Begin
 		
     if id_subscriptor = v_id_subscriptor then
     		update bd_sample.tbl_subscriptores
-			set v_id_subscriptor = id_subscriptor;
-			set v_codigo_subscriptor = codigo_subscriptor;
-			set v_nombres = nombres;
-			set v_apellidos = apellidos; 
+				set id_subscriptor = v_id_subscriptor;
+				set codigo_subscriptor = v_codigo_subscriptor;
+				set nombres = v_nombres;
+				set apellidos = v_apellidos; 
+			where id_subscriptor = v_id_subscriptor;
 	else     
 		insert into bd_sample.tbl_subscriptores ( 
 				id_subscriptor, codigo_subscriptor, nombres, apellidos
@@ -55,8 +56,8 @@ Create Procedure SP_Guardar_Producto(
 	in p_id_producto 	int,
     in p_nombre			varchar(45),
     in p_descripcion	varchar(45),
-    in p_precio_costo 	decimal(12,2),
-    out p_precio_venta	decimal(12,2),
+    in p_precio_costo 	decimal(12,2) default 0,
+    in p_precio_venta	decimal(12,2)default 0,
     in p_id_hist 		int,
     in p_fecha_insercion datetime
 )
@@ -74,11 +75,10 @@ Begin
 	set v_nom_prod			= p_nombre; 
 	set v_desc_prod			= p_descripcion;
 	set v_precio_costo		= p_precio_costo;
-    set v_precio_venta 		= p_precio_venta;
+    set v_precio_venta 		= p_precio_venta * 1.25;
     set v_id_hist			= p_id_hist;
     set v_fecha_insercion	= p_fecha_insercion;
-		
-	set p_precio_venta = (p_precio_costo)*1.25;
+	
         
 	if id_producto = v_id_producto then
 		case
@@ -87,6 +87,18 @@ Begin
 			when v_precio_venta >= 8 then set Porcentaje ='60%';
 			else set Porcentaje ='[Indefinido]';
 		end case;
+        
+        update bd_sample.tbl_productos
+			set id_producto = v_id_producto;
+			set nombre = v_nombre;
+			set descripcion = v_descripcion;
+			set v_precio_costo = v_precio_costo;
+			set v_precio_venta = v_precio_venta; 
+			
+		update bd_sample.tbl_productos_hist
+			set id_hist = v_id_hist;
+			set id_producto = v_id_producto;
+			set fecha_insercion = v_fecha_insercion;
 	else
 		insert into bd_sample.tbl_productos_hist ( 
 			id_hist, id_producto, fecha_insercion  
@@ -94,19 +106,7 @@ Begin
 			v_id_hist, v_id_producto, v_fecha_insercion 
 		);
     end if;
-    
-    update bd_sample.tbl_productos
-		set v_id_producto = id_producto;
-        set v_nombre = nombre;
-        set v_descripcion = descripcion;
-        set v_precio_costo = precio_costo;
-        set v_precio_venta = precio_venta; 
-		
-	update bd_sample.tbl_productos_hist
-		set v_id_hist = id_hist;
-        set v_id_producto = id_producto;
-        set v_fecha_insercion = fecha_insercion;
-        
+
 	commit;
 End;
 
@@ -119,9 +119,9 @@ Create Procedure SP_Guardar_Factura(
     in p_fecha_emision	datetime,
     in p_id_subscriptor	int,
     in p_numero_items 	int,
-    out p_isv_total		decimal(12,2),
-	out p_subtotal		decimal(12,2),
-    out p_totapagar		decimal(12,2),
+    in p_isv_total		decimal(12,2),
+	in p_subtotal		decimal(12,2),
+    in p_totapagar		decimal(12,2),
     in p_precio_costo 	decimal(12,2)
 )
 
@@ -134,27 +134,27 @@ Begin
     declare v_isv_total			decimal(12,2);
     declare v_subtotal			decimal(12,2);
     declare v_totapagar			decimal(12,2);
-    declare v_precio_costo 		decimal(12,2);
-
+    
 	
 	set v_id_factura 		= p_id_factura;
-	set v_fecha_emision		= curdate();
+	set v_fecha_emision		= p_fecha_emision;
 	set v_id_subscriptor	= p_id_subscriptor;
 	set v_numero_items 		= p_numero_items;
-    set v_isv_total 		= (p_numero_items*p_precio_costo)*0.15;
-    set v_subtotal			= (p_numero_items*p_precio_costo);
-    set v_totapagar 		= (p_numero_items*p_precio_costo)*1.15;
+    set v_isv_total 		= p_isv_total;
+    set v_subtotal			= p_subtotal;
+    set v_totapagar 		= p_totapagar;
 
         
 	if id_factura = v_id_factura then
 		 update bd_sample.tbl_facturas 
-				set v_id_factura 		= id_factura;
-				set fecha_emision		= curdate();
-				set v_id_subscriptor	= id_subscriptor;
-				set v_numero_items 		= numero_items;
-				set isv_total 			= (p_numero_items*p_precio_costo)*0.15 ;
-				set subtotal			= (p_numero_items*p_precio_costo);
-				set totapagar 			= (p_numero_items*p_precio_costo)*1.15;
+				set id_factura 			= v_id_factura;
+				set fecha_emision		= v_fecha_emision;
+				set id_subscriptor		= v_id_subscriptor;
+				set numero_items 		= v_numero_items;
+				set isv_total 			= v_isv_total;
+				set subtotal			= v_subtotal;
+				set totapagar 			= v_totapagar;
+			where id_factura = v_id_factura;
 	else
 		insert into bd_sample.tbl_facturas ( 
 				id_factura, fecha_emision, id_subscriptor, numero_items, isv_total, subtotal, totapagar  
@@ -171,13 +171,6 @@ Drop Procedure if Exists SP_Procesar_Factura;
 Delimiter //
 Create Procedure SP_Procesar_Factura(
 	in p_id_factura 	int,
-    in p_fecha_emision	datetime,
-    in p_id_subscriptor	int,
-    in p_numero_items 	int,
-    out p_isv_total		decimal(12,2),
-    out p_subtotal		decimal(12,2),
-    out p_totapagar		decimal(12,2),
-    in p_precio_costo 	decimal(12,2),
     in p_id_producto 	int,
     in p_cantidad 		int
     )
@@ -185,24 +178,21 @@ Create Procedure SP_Procesar_Factura(
 Begin
 /* Crear Proceso de Facturación */
     declare v_id_factura 		int;
-    declare v_fecha_emision		datetime;
-    declare v_id_subscriptor	int;
-    declare v_numero_items 		int;
-    declare v_isv_total			decimal(12,2);
-    declare v_subtotal			decimal(12,2);
-    declare v_totapagar			decimal(12,2);
-    declare v_precio_costo 		decimal(12,2);
     declare v_id_producto		int;
     declare v_cantidad			int;
+	declare v_numero_items 	    int;
+	declare v_isv      			decimal(12,2) default 0;
+	declare v_total   			decimal(12,2) default 0;
+	declare v_subtotal     		decimal(12,2) default 0;
+	declare v_precio_venta      decimal(12,2) default 0;
 
     set v_id_factura 		= p_id_factura;
     set v_id_producto       = p_id_producto;
     set v_cantidad          = p_cantidad;
-	set v_precio_costo      = p_precio_costo;
-	set v_isv_total 		= (p_numero_items*p_precio_costo)*0.15;
-	set v_subtotal			= (p_numero_items*p_precio_costo);
-	set v_totapagar 		= (p_numero_items*p_precio_costo)*1.15;
-
+	
+    select precio_venta into v_precio_venta  
+	from bd_sample.tbl_productos 
+	where id_producto = v_id_producto; 
 
 /* Crear la factura */
 
@@ -212,15 +202,18 @@ Begin
 		v_id_factura, v_id_producto, v_cantidad
     );
 
+	set v_subtotal	= (v_precio_venta * v_cantidad);
+    set v_isv		= (v_subtotal * 0.15);
+    set v_total	= v_subtotal + v_isv; 
+    
 /* Update Facturas */
-	update bd_sample.tbl_facturas 
-		set v_id_factura 		= id_factura;
-		set fecha_emision		= curdate();
-		set v_id_subscriptor	= id_subscriptor;
-		set v_numero_items 		= numero_items;
-		set isv_total 			= (p_numero_items*p_precio_costo)*0.15 ;
-		set subtotal			= (p_numero_items*p_precio_costo);
-		set totapagar 			= (p_numero_items*p_precio_costo)*1.15;
+	update  bd_sample.tbl_facturas Set 
+		numero_items 	= 	numero_items + v_cantidad,
+		fecha_emision 	=	now(),
+		isv_total   	=  	isv_total + v_isv,
+		subtotal    	=   subtotal  + v_subtotal,
+		totapagar		=   totapagar + v_total
+	where id_factura = v_id_factura; 
 	
 	commit; 
 End; 
