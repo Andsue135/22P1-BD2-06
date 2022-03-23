01f337e6209eb1e2a2e07596494176ec0cc305c1
Danmakora <joshegalaxy11@gmail.com>
2022-03-23 01:18:32 -0600

    Ejercicio No. 1 Complete

--- Proyecto BD2/Ejercicio No. 1 Proyecto.sql
+++ Proyecto BD2/Ejercicio No. 1 Proyecto.sql
@@ -1,26 +1,31 @@
-Drop Function Valor_Tarifas;
+Drop Function if exists bd_platvideo.Valor_Tarifas;
 DELIMITER //
 Create Function bd_platvideo.Valor_Tarifas (
 	id_tarifa int,
-    p_valor Decimal(12,2)
+    valor_numero decimal(12,2)
 )Returns Decimal(12,2) Deterministic
 BEGIN
 	declare v_id_tarifa int;
-    declare v_id_valor decimal(12,2);
-    
-    set v_valor = p_valor;
+    declare v_valor decimal(12,2) default 0;
+    declare v_valorA decimal(12,2);
+    declare v_valorB decimal(12,2);
+    declare VN decimal(12,2);
+      
     set v_id_tarifa = id_tarifa;
+    set VN = valor_numero;
     
-    if v_valor > 1 and v_valor <= 15.00 then
-    select valor_real into v_valor from bd_platvideo.tbl_tarifas where id_tarifa = v_id_tarifa order by ABS(valor_real-v_valor) LIMIT 1;
-    
-    elseif v_valor < 1 then
-    select valor_procentual into v_valor from bd_platvideo.tbl_tarifas where id_tarifa = v_id_tarifa order by ABS(valor_procentual-v_valor) LIMIT 1;
-    
+    if VN > 1 and VN <= 15.00 then
+		set v_valorA = (select valor_real from bd_platvideo.tbl_tarifas where id_tarifa = v_id_tarifa order by ABS(valor_real -v_valor));
+		return v_valorA;
+    elseif VN < 1 then
+		set v_valorB = (select valor_procentual from bd_platvideo.tbl_tarifas where id_tarifa = v_id_tarifa order by ABS(valor_procentual -v_valor));
+		return v_valorB;
     else
-    select valor_real, valor_procentual from bd_platvideo.tbl_tarifas where id_tarifa = v_id_tarifa;
+		set v_valor = VN;
+		return v_valor;
     end if;
     
-    return v_valor;
 END //
 DELIMITER ;
+
+select bd_platvideo.Valor_Tarifas(3,25) from bd_platvideo.tbl_tarifas;
